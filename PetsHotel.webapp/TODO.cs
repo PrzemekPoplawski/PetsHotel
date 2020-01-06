1eef7815a460df2cbf868e72cf9fab965919925a
bolix <bolix@DESKTOP-J7M1U6T>
2020-01-06 21:43:47 +0100

    dodanie classy z rzeczami to do

---
+++ PetsHotel.webapp/TODO.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PetsHotel.webapp
+{
+    public class TODO
+    {
+//- nr telefonu tylko numerem! (walidacja numeru - Regex Match)
+//- po rejestracji przekieruj do strony zalogowania
+//- wygląd zarejestruj - login, hasło, potwierdź hasło 
+//- logowanie przez email ? login , email ----> 
+//- sprawdzenie przy rejestracji czy istnieje dany login, to samo z emailem przy wypełnianiu
+//- custom error site strona przy bledzie(Przepraszamy sprobuj ponownie),
+//- validacja przy logowaniu,
+
+// dodawanie zwierzaka:
+// - widok, 
+// dla każdego użytkownika lista zwierzakó z możliwością edycji,
+// mozliwosc edycji jako json result (wyskakujące okno)
+// usunięcie zwierzaka (xDel)
+
+// dowiedziec się o zapisywaniu obrazów w formie bitowej,
+// nowa tabela, 
+
+// po zrobieniu modułu ze zwierzakami: moduł dodawania ogłoszeń
+// interfejs
+    }
+}
