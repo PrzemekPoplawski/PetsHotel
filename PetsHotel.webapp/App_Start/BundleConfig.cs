4d18b1b0c5bf63ee2dfaa56c6f35c3c6acf6911e
bolix <bolix@DESKTOP-J7M1U6T>
2020-01-14 22:24:52 +0100

    dodanie data table

--- PetsHotel.webapp/App_Start/BundleConfig.cs
+++ PetsHotel.webapp/App_Start/BundleConfig.cs
@@ -17,14 +17,23 @@ namespace PetsHotel.webapp
             // Użyj wersji deweloperskiej biblioteki Modernizr do nauki i opracowywania rozwiązań. Następnie, kiedy wszystko będzie
             // gotowe do produkcji, użyj narzędzia do kompilowania ze strony https://modernizr.com, aby wybrać wyłącznie potrzebne testy.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+                        "~/Scripts/modernizr-*",
+                        "~/Scripts/MyScripts/datatables.js",
+                        "~/Scripts/MyScripts/DataTables-1.10.20/js/dataTables.*",
+                        "~/Scripts/MyScripts/DataTables-1.10.20/js/jquery.dataTables.js"
+                        //"~/Scripts/MyScripts/datatables.min.js",
+                        ));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/DataTables/datatables.css",
+                      "~/Content/DataTables/css/dataTables*",
+                      "~/Content/DataTables/css/jquery.dataTables.css"));
+
         }
     }
 }
