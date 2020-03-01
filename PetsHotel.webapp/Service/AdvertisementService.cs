4566b0723422c8d225164b6c2ea46b92fafc8dfe
bolix <bolix@DESKTOP-J7M1U6T>
2020-03-01 17:38:48 +0100

    dodawanie ogloszen

---
+++ PetsHotel.webapp/Service/AdvertisementService.cs
@@ -0,0 +1,38 @@
+﻿using PetsHotel.webapp.Entity;
+using PetsHotel.webapp.Repositories;
+using PetsHotel.webapp.ViewModels.Advertisement;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PetsHotel.webapp.Service
+{
+    public class AdvertisementService : IAdvertisementService
+    {
+        private readonly IRepository<AdvertisementEntity> _advertisementRepository;
+
+        public AdvertisementService(IRepository<AdvertisementEntity> advertisementRepository)
+        {
+            _advertisementRepository = advertisementRepository;
+        }
+
+        public void AddAdvertisement(AdvertisementEntity advertisementEntity)
+        {
+            _advertisementRepository.Add(advertisementEntity);
+        }
+
+        public IQueryable<AdvertisementEntity> GetAllAdvertisement()
+        {
+            return _advertisementRepository.GetAllValues();
+        }
+
+        public void Save()
+        {
+            _advertisementRepository.Save();
+        }
+
+    }
+
+
+}
---
+++ PetsHotel.webapp/Service/IAdvertisementService.cs
@@ -0,0 +1,16 @@
+﻿using PetsHotel.webapp.Entity;
+using PetsHotel.webapp.ViewModels.Advertisement;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PetsHotel.webapp.Service
+{
+    public interface IAdvertisementService
+    {
+        void AddAdvertisement(AdvertisementEntity advertisementTemplate);
+        IQueryable<AdvertisementEntity> GetAllAdvertisement();
+        void Save();
+    }
+}
